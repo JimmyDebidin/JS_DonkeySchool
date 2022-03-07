a1b4a2176f920baf8d0625ff8a40869b85242d5d
Jimmy Debidin <96825957+JimmyDebidin@users.noreply.github.com>
2022-03-07 20:08:14 +0100

    challenge1 finish

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+"use strict";
+/* Déclarer 2 variables: admin et name.
+Assigner la valeur “John” à name.
+Copier la valeur de name à admin.
+Afficher la valeur de admin sur la console. */
 
+let name = "John";
+let admin = name;
+console.log(admin);
