6ecdbed2813859aaabe7e63400db03fdaf0451e7
Jimmy Debidin <96825957+JimmyDebidin@users.noreply.github.com>
2022-03-10 11:07:44 +0100

    finish challenge5

--- index.js
+++ index.js
@@ -1,32 +1,19 @@
 "use strict";
 
-/* Challenge 4*/
+/* Challenge 5*/
+let tips;
+let totalTips;
+let bill = prompt("Valeur de la note ?");
+(bill >= 50 && bill <= 300) ? (tips = 0.15, totalTips = bill * tips) : tips = 0.20, totalTips = (bill * tips); 
+console.log("La note était de " + bill + ", le pourboire de " + totalTips + " et la valeur totale était de " + (+bill + totalTips));
 
-let AverageDauphins1 = (96 + 108 + 89) / 3;
-let AverageDauphins2 = (97 + 112 + 101) / 3;
-let AverageDauphins3 = (97 + 112 + 101) / 3;
-let AverageKoalas1 = (88 + 91 + 110) / 3;
-let AverageKoalas2 = (109 + 95 + 123) / 3;
-let AverageKoalas3 = (109 + 95 + 106) / 3;
-let overallAverageDauphins = (AverageDauphins1 + AverageDauphins2 + AverageDauphins3);
-let overallAverageKoalas = (AverageKoalas1 + AverageKoalas2 + AverageKoalas3);
-let winner;
+/* Classic way
 
-if (overallAverageDauphins > overallAverageKoalas && overallAverageDauphins > 100) {
-    
-    winner = "Dauphins";
-} else if (overallAverageDauphins === overallAverageKoalas && overallAverageDauphins > 100) {
-
-    winner = "égalité les deux équipes remportent la compétition";
-} else if (overallAverageKoalas > overallAverageDauphins && overallAverageKoalas > 100) {
-    winner = "Koalas";
+if (bill >= 50 && bill <= 300) {
+    tips = 0.15;
+    totalTips = (bill * tips);
 } else {
-
-    winner = "Aucun gagnant";
+    tips = 0.20;
+    totalTips = (bill * tips);
 }
-
-console.log(winner);
-
-
- 
-
+ */
