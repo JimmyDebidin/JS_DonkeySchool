73639048c4d0941fd6d9f683502e74937389de3b
Jimmy Debidin <96825957+JimmyDebidin@users.noreply.github.com>
2022-03-10 10:06:37 +0100

    Challenge4 finish

--- index.js
+++ index.js
@@ -1,84 +1,31 @@
 "use strict";
 
-/* Challenge 3 */
+/* Challenge 4*/
 
-let heightBernard1 = 1.69;
-let weightBernard1 = 78;
-let heightMarcel1 = 1.95;
-let weightMarcel1 = 92;
-let heightBernard2 = 1.88;
-let weightBernard2 = 95;
-let heightMarcel2 = 1.76;
-let weightMarcel2 = 85;
-let imcBernard1 = weightBernard1 / (heightBernard1 * heightBernard1);
-let imcBernard2 = weightBernard2 / (heightBernard2 * heightBernard2);
-let imcMarcel1 = weightMarcel1 / (heightMarcel1 * heightMarcel1);
-let imcMarcel2 = weightMarcel2 / (heightMarcel2 * heightMarcel2);
-let bernardHigherIMC;
+let AverageDauphins1 = (96 + 108 + 89) / 3;
+let AverageDauphins2 = (97 + 112 + 101) / 3;
+let AverageDauphins3 = (97 + 112 + 101) / 3;
+let AverageKoalas1 = (88 + 91 + 110) / 3;
+let AverageKoalas2 = (109 + 95 + 123) / 3;
+let AverageKoalas3 = (109 + 95 + 106) / 3;
+let overallAverageDauphins = (AverageDauphins1 + AverageDauphins2 + AverageDauphins3);
+let overallAverageKoalas = (AverageKoalas1 + AverageKoalas2 + AverageKoalas3);
+let winner;
 
-if (imcBernard1 > imcMarcel1) {
-     
-    bernardHigherIMC = true;
-    console.log("Bernard a un IMC " + imcBernard1 + " plus élevé que Marcel " + imcMarcel1);
-    if (imcBernard1 < 22) {
-        console.log("Bernard : insuffisance pondérale");
-    } else if (imcBernard1 >= 22 && imcBernard1 <= 27) {
-        console.log("Bernard : poids normal");
-    } else if (imcBernard1 > 27 && imcBernard1 <= 32) {
-        console.log("Bernard : surpoids");
-    } else if (imcBernard1 > 32) {
-        console.log("Bernard : obésité");
-        }
-   } else {
+if (overallAverageDauphins > overallAverageKoalas && overallAverageDauphins > 100) {
+    
+    winner = "Dauphins";
+} else if (overallAverageDauphins === overallAverageKoalas && overallAverageDauphins > 100) {
 
-    bernardHigherIMC = false;
-    console.log("Marcel a un IMC " + imcMarcel1 + " plus élevé que Bernard " + imcBernard1);
-    if (imcMarcel1 < 22) {
-        console.log("Marcel : insuffisance pondérale");
-    } else if (imcMarcel1 >= 22 && imcMarcel1 <= 27) {
-        console.log("Marcel : poids normal");
-    } else if (imcMarcel1 > 27 && imcMarcel1 <= 32) {
-        console.log("Marcel : surpoids");
-    } else if (imcMarcel1 > 32) {
-        console.log("Marcel : obésité");
-        }
+    winner = "égalité les deux équipes remportent la compétition";
+} else if (overallAverageKoalas > overallAverageDauphins && overallAverageKoalas > 100) {
+    winner = "Koalas";
+} else {
+
+    winner = "Aucun gagnant";
 }
 
-if (imcBernard2 > imcMarcel2) {
-
-    bernardHigherIMC = true;
-    console.log("Bernard a un IMC " + imcBernard2 + " plus élevé que Marcel " + imcMarcel2);
-     if (imcBernard2 < 22) {
-        console.log("Bernard : insuffisance pondérale");
-    } else if (imcBernard2 >= 22 && imcBernard2 <= 27) {
-        console.log("Bernard : poids normal");
-    } else if (imcBernard2 > 27 && imcBernard2 <= 32) {
-        console.log("Bernard : surpoids");
-    } else if (imcBernard2 > 32) {
-        console.log("Bernard : obésité");
-        }
-   } else {
-
-    bernardHigherIMC = false;
-    console.log("Marcel a un IMC " + imcMarcel2 + " plus élevé que Bernard " + imcBernard2);
-    if (imcMarcel2 < 22) {
-        console.log("Marcel : insuffisance pondérale");
-    } else if (imcMarcel2 >= 22 && imcMarcel2 <= 27) {
-        console.log("Marcel : poids normal");
-    } else if (imcMarcel2 > 27 && imcMarcel2 <= 32) {
-        console.log("Marcel : surpoids");
-    } else if (imcMarcel2 > 32) {
-        console.log("Marcel : obésité");
-        }
-}
-
-
-
-
-
-
-
-  
+console.log(winner);
 
 
  
