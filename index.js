898e20fb4bdd622e812cc74b23c1b51790c1b030
Jimmy Debidin <96825957+JimmyDebidin@users.noreply.github.com>
2022-03-12 14:56:19 +0100

    Finish challenge9

--- index.js
+++ index.js
@@ -1,45 +1,33 @@
 "use strict";
 
-/* Challenge 8*/
+/* Challenge 9*/
 
+/* // Welcome to DonkeyDecode, your mission is to decode the full message. Good luck
 
-
-const calcTip = (notes) => {
-    if (notes >= 50 && notes <= 300) {
-        let tips = (notes * 0.15);
-        return(tips);
-
-  } else {
-    let tips = (notes * 0.20);
-    return(tips);  
+// EX 01 -  Use a loop to remove the 'X' and use console.log to reveal the message 
+const hiddenMessage = ["X", "X", "X", "X", "W", "X", "E",  "X", "X", "X", "X", "X", "L", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "L", "X", "X", "X", "X", "X", "X", "X", "X", "X", " ","X", "X", "X", "X", "X", "X", "X", "X", "D", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "O", "X", "X", "X", "X", "X", "X", "N", "X", "X", "X", "X", "E", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", ",", "X", " ", "X"]
+/*  */
+const hiddenMessage = ["X", "X", "X", "X", "W", "X", "E",  "X", "X", "X", "X", "X", "L", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "L", "X", "X", "X", "X", "X", "X", "X", "X", "X", " ","X", "X", "X", "X", "X", "X", "X", "X", "D", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "O", "X", "X", "X", "X", "X", "X", "N", "X", "X", "X", "X", "E", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", ",", "X", " ", "X"]
+let revealMessage = '';
+for (let i = 0; i < hiddenMessage.length; i++){
+    if (hiddenMessage[i] !== "X") {
+        revealMessage = revealMessage + hiddenMessage[i];
     }
 }
 
-const notes = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-const tips = [];
-const totals = [];
+console.log(revealMessage);
 
-/* Send data in arrays */
+// EX 02 - Remove the 'X' and the even numbers
+// Hint: As this is a string you should find a way to convert into an array.
 
-for (let i = 0; i < notes.length; i++) {
-    calcTip(notes[i]);
-    tips.push(calcTip(notes[i]));
-    totals.push(calcTip(notes[i]) + notes[i]);
-}
+const hiddenCode = "XXXXXXXXXXXXXXXXXXXXXXXXXXXCXXXXXXXOXXXXXDXXXXXXXXXXXXXXXEXXXX XXXXXXXXXXXXXXX:X0XXXXXXX2XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX3XXXXXX2XXXXXXXXX2XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX4XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX6XXXXXXXXXXXXXXX6XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX7XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX2XXXXXXXXXX8XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX8XXXXXXXXXXX4XXXXXXXXXXXX6XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX4XXXXXXXXXXXXXXXXXXX9XXXX9XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX2XXX4XXXXXXXXXXX6XXXXXXXXX2XXXXXXXX3XXXXXXXX2XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX4XXXXX"
+let revealCode = '';
+const arrayHiddenCode = (hiddenCode.split(''));
 
-console.log(notes, tips, totals);
-
-/* returns the average of an array */
-
-let sum = 0;
-const calcAverage = (array) => {
-    for (let i = 0; i < array.length; i++) {
-    sum = sum + array[i]; 
+for (let i = 0; i < arrayHiddenCode.length; i++) {
+    if (arrayHiddenCode[i] !== "X" && arrayHiddenCode[i] % 2 !== 0) {
+        revealCode = revealCode + arrayHiddenCode[i];
     }
-   return ((sum / array.length));
 }
 
-console.log(calcAverage(totals));
-
-
-
+console.log(revealCode);
