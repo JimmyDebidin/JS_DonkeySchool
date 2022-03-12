eb1c452fb71534f884fcb4044156c05fa62e7576
Jimmy Debidin <96825957+JimmyDebidin@users.noreply.github.com>
2022-03-12 12:39:52 +0100

    Finish challenge7

--- index.js
+++ index.js
@@ -1,49 +1,46 @@
 "use strict";
 
-/* Challenge 6*/
+/* Challenge 7*/
 
 
-/* De retour avec nos équipes de gymnastique : Les Dauphins et les Koalas ! 
-Il y’a une nouvelle discipline qui fonctionne différemment.
-
-Chaque équipe joue 3 fois, ensuite la moyenne des 3 scores est calculée (donc une moyenne par équipe).
-
-Une équipe gagne seulement si elle a au moins le double de la moyenne de score de l’autre équipe. 
-Sinon, personne ne gagne !
+/* Mitch veut continuer son programme qui calcule le pourboire au resto. 
+Rappel : dans son pays, généralement, c’est 15% de la facture totale 
+lorsque celle-ci est entre 50 et 300. Si la valeur est différente, le pourboire est de 20%.
 
 Tâches :
-Créer une arrow function “calcAverage” qui calcule la moyenne des 3 scores.
-Utiliser la fonction pour calculer la moyenne de chaque équipe.
-Créer une fonction “checkWinner” qui prend les 2 moyennes de scores et qui log 
-dans la console l’équipe gagnante : “L’équipe Koala gagne (30 vs 13)”.
-Utiliser la fonction “checkWinner”.
-Ignorer l’égalité cette fois-ci.
+Écrire une fonction ‘calcTip’ qui prend la valeur de la note en paramètre 
+et retourne le pourboire correspondant. Testez votre fonction avec une note de 100 euros.
+Et maintenant, utilisons des tableaux ! Créer un tableau ‘bills’ contenant les données de test
+ en dessous.
+Créez un tableau ‘tips’ contenant la valeur du pourboire de chaque note. 
+Calculé grâce à la fonction ‘calcTip’ que vous avez créé avant.
+Créer un tableau ‘totals’ contenant les valeurs des totaux (note + pourboire).
 Données de test :
-Score Dauphins : 44, 23, 71 / Score Koalas : 65, 54, 49
-Score Dauphins : 85, 54, 41 / Score Koalas : 23, 34, 27 */
+Valeur de la note : 125, 555 et 44 */
 
-const calcAverage = (score1, score2, score3) => ((score1 + score2 + score3) / 3);
+const calcTip = (bills) => {
+    if (bills >= 50 && bills <= 300) {
+        let tips = (bills * 0.15);
+        return(tips);
 
-const overallAverageDauphins1 = calcAverage(44, 23, 71);
-const overallAverageDauphins2 = calcAverage(85, 54, 41);
-const overallAverageKoalas1 = calcAverage(65, 54, 49);
-const overallAverageKoalas2 = calcAverage(23, 34, 27);
-let team1 = 'Dauphins';
-let team2 = 'Koalas';
-
-function checkWinner(overallAverageTeam1, overallAverageTeam2) {
-
-    if (overallAverageTeam1 > overallAverageTeam2) {
-        console.log(`L'équipe ${team1} gagne (${overallAverageTeam1} vs ${overallAverageTeam2})`);
-
-    } else if (overallAverageTeam1 === overallAverageTeam2){
-        console.log("égalité");
-
-     } else {
-        console.log(`L'équipe ${team2} gagne (${overallAverageTeam2} vs ${overallAverageTeam1})`);
+  } else {
+    let tips = (bills * 0.20);
+    return(tips);  
     }
 }
 
-checkWinner(overallAverageDauphins1, overallAverageKoalas1);
-checkWinner(overallAverageDauphins2, overallAverageKoalas2);
-checkWinner(50, 50);
+const bills = [125, 555, 44];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+    calcTip(bills[i]);
+    tips.push(calcTip(bills[i]));
+    totals.push(calcTip(bills[i]) + bills[i]);
+}
+
+console.log(bills, tips, totals);
+
+
+
+
